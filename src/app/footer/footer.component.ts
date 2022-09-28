875856b50094836e3d51f342992df07b8e633a55
Arun Vijayarengan <arunprabu.v@hotmail.com>
2022-09-28 12:30:42 +0530

    updated

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -5,6 +5,11 @@ import { Component, OnInit } from '@angular/core';
   template: `
     <footer class="text-center">
       <hr>
+      <app-menu>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Back to Top</a>
+        </li>
+      </app-menu>
       <p>Copyright 2022 | Arun</p>
     </footer>
   `,
