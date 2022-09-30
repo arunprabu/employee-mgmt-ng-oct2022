adca9b8e1e5966191973caab2dc9b254afefd80d
Arun Vijayarengan <arunprabu.v@hotmail.com>
2022-09-30 16:30:12 +0530

    updated

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -10,6 +10,7 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component'; // Taking up the comp for testing
 
+// group of related test specs - TEST SUITE
 describe('AppComponent', () => {
 
   // When
